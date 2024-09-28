4af27437d4f4882848a85df7fbf12e76151eff23
Marcelo Menegheti <marcelomenegheti18@gmail.com>
2024-09-27 22:47:15 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -41,7 +41,11 @@ public class Program {
 		
 		System.out.println("Inserted! New id = " + newSeller.getId());
 
-		
+		System.out.println("\n---- TEST 5: seller update ----");
+		seller = sellerDao.findById(1);
+		seller.setName("Marcus Brito");
+		sellerDao.update(seller);
+		System.out.println("Update completed! ");
 	}
 
 }
